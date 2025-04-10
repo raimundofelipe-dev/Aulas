16d0a3966fb32936183a14fd61cb0420dfa0483d
raimundofelipe-dev <rdfelipedev@gmail.com>
2025-04-10 10:13:18 -0300

    Ajusta Conta.php com melhorias no código

--- aulas/o-obj/conta.php
+++ aulas/o-obj/conta.php
@@ -2,7 +2,16 @@
 
 class Conta 
 {
-
+    //definir dados da conta
+   public string $cpfTitular;
+   public string $nomeTitular;
+   public float $saldo;
 }
- $umaNovaConta = new Conta();
+
+$primeiraConta = new Conta();
+$primeiraConta->cpfTitular = '630.888.888-00';
+$primeiraConta->nomeTitular = 'Felipe Oliveira';
+$primeiraConta->saldo = 10000;
+var_dump($primeiraConta);
+
 ?>
